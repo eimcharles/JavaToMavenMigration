edcfd25a1282c42ffd9c6f27ce6c4f62995837d0
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-01 14:26:09 -0500

    Included the core Main class with the necessary main method, and added documentation explaining its role as the JVM entry point in the context of the Maven build.

--- src/main/java/com/eimc/Main.java
+++ src/main/java/com/eimc/Main.java
@@ -1,5 +1,23 @@
 package com.eimc;
 
+/**
+ *      The primary role of the Main class in
+ *      this Maven project is to serve as the
+ *      single, defined entry point
+ *      for the Java Virtual Machine (JVM).
+ *
+ *      1.Execution Start: It contains the
+ *      'public static void main(String[] args)'
+ *      method, which is the first code
+ *      executed when the application is run.
+ *
+ *      2. Executable Artifact: In the context of Maven,
+ *      the main class is explicitly declared within the
+ *      configuration of the maven-jar-plugin (in the pom.xml)
+ *      using the mainClass tag, which tells the final
+ *      executable JAR file where to begin execution.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
