32a033000802e8d59c5719f5062ae6fc1ae5cd76
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-01 14:36:06 -0500

    Included UserTest class responsible for testing the functionality of the User.java production class (which should reside in src/main/java).

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,20 @@
+
+/**
+ *          UserTest: Unit Test Class
+ *          Purpose: This class is responsible for testing the functionality of the
+ *          'User.java' production class (which should reside in src/main/java).
+ *
+ *          Naming Convention:
+ *          - The name 'UserTest' adheres to the standard Maven/JUnit convention (ProductionClassName + Test).
+ *          - Maven's build system (specifically the maven-surefire-plugin) automatically
+ *            discovers and executes all classes ending in 'Test.java' during the 'mvn test' goal.
+ *
+ *          Location:
+ *          - This file must be located in the src/test/java directory structure
+ *          (e.g., src/test/java/com/eimc/UserTest.java) to be processed by Maven.
+ */
+
+public class UserTest {
+
+
+}
