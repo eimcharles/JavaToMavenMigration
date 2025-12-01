f9e8f293d918ab77327947bab48490078cd35466
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-01 14:49:47 -0500

    Changing the comment for the file path in UserTest for more clarity.

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -2,7 +2,7 @@
 /**
  *          UserTest: Unit Test Class
  *          Purpose: This class is responsible for testing the functionality of the
- *          'User.java' production class (which should reside in src/main/java).
+ *          'User.java' production class (which should reside in src/main/java/com/eimc).
  *
  *          Naming Convention:
  *          - The name 'UserTest' adheres to the standard Maven/JUnit convention (ProductionClassName + Test).
